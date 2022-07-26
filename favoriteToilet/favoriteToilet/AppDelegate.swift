cddb4c0c758c826a49b12713a6e9afd773403109
sungju-kim <wnwn0719@gmail.com>
2022-07-26 13:35:19 +0900

    Add: 프로젝트 생성 및 초기 주석제거

---
+++ favoriteToilet/favoriteToilet/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  favoriteToilet
+//
+//  Created by dale on 2022/07/26.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
---
+++ favoriteToilet/favoriteToilet/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MainViewController.swift
+//  favoriteToilet
+//
+//  Created by dale on 2022/07/26.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
