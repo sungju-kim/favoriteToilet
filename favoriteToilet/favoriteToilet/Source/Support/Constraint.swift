1d96bf24ba0ddc833970678b8892a024d85cf286
sungju-kim <wnwn0719@gmail.com>
2022-07-26 16:30:53 +0900

    Add: default Constraint 추가

---
+++ favoriteToilet/favoriteToilet/Source/Support/Constraint.swift
@@ -0,0 +1,14 @@
+//
+//  Constraint.swift
+//  favoriteToilet
+//
+//  Created by dale on 2022/07/26.
+//
+
+import UIKit
+
+enum Constraint {
+    static let min: CGFloat = 8
+    static let regular: CGFloat = 16
+    static let max: CGFloat = 32
+}
