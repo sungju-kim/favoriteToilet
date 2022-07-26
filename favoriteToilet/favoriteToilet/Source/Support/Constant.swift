f7c0347b082d881a083e733387660eafaf03d303
sungju-kim <wnwn0719@gmail.com>
2022-07-26 16:31:46 +0900

    Add: Constant 및 LogInView 상수 추가

---
+++ favoriteToilet/favoriteToilet/Source/Support/Constant.swift
@@ -0,0 +1,18 @@
+//
+//  Constant.swift
+//  favoriteToilet
+//
+//  Created by dale on 2022/07/26.
+//
+
+import Foundation
+
+// MARK: - LogInView
+extension String {
+    static let slogan = "달라진것은 단 하나,\n화장실 입니다."
+
+    enum LogInView {
+        static let appleLogIn = "Apple 계정으로 로그인"
+        static let findToilet = "화장실 찾기"
+    }
+}
