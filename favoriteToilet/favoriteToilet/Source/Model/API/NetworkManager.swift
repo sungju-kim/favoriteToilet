5a559d004b2ce8786731c81a9810fdea19832a5c
sungju-kim <wnwn0719@gmail.com>
2022-07-27 16:14:58 +0900

    Add: NetworkManager 구현
    #17

---
+++ favoriteToilet/favoriteToilet/Source/Model/API/NetworkManager.swift
@@ -0,0 +1,46 @@
+//
+//  NetworkManager.swift
+//  favoriteToilet
+//
+//  Created by dale on 2022/07/27.
+//
+
+import Foundation
+import RxSwift
+import Alamofire
+
+final class NetworkManager<T: Requestable> {
+    private init() {}
+
+    func request(endPoint: Requestable) -> Observable<T> {
+        return Observable.create { observer in
+            guard let url = endPoint.url else {
+                observer.onError(NetworkError.invalidURL)
+                return Disposables.create {}
+            }
+
+            let headers: HTTPHeaders = endPoint.headers
+
+            let request: DataRequest = AF.request(url,
+                                                  method: endPoint.method,
+                                                  parameters: endPoint.parameter,
+                                                  headers: headers)
+
+            request
+                .validate(statusCode: 200..<300)
+                .response { response in
+                    guard let data = response.data else {
+                        observer.onError(NetworkError.emptyData)
+                        return
+                    }
+                    guard let decodedData = try? JSONDecoder().decode(T.self, from: data) else {
+                        observer.onError(NetworkError.failToDecode)
+                        return
+                    }
+                    observer.onNext(decodedData)
+                    observer.onCompleted()
+                }
+            return Disposables.create {}
+        }
+    }
+}
