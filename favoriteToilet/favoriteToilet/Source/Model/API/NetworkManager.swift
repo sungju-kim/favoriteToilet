e98a9e90a61e5c1bf539716060eb9a67256ae598
sungju-kim <wnwn0719@gmail.com>
2022-07-28 17:51:32 +0900

    Add: Request Protocol 생성 및 NetworkManager 수정

--- favoriteToilet/favoriteToilet/Source/Model/API/NetworkManager.swift
+++ favoriteToilet/favoriteToilet/Source/Model/API/NetworkManager.swift
@@ -9,10 +9,10 @@ import Foundation
 import RxSwift
 import Alamofire
 
-final class NetworkManager<T: Requestable> {
+class NetworkManager: RequestProtocol {
     private init() {}
 
-    func request(endPoint: Requestable) -> Observable<T> {
+    func request<T: Decodable>(endPoint: Requestable) -> Observable<T> {
         return Observable.create { observer in
             guard let url = endPoint.url else {
                 observer.onError(NetworkError.invalidURL)
@@ -41,6 +41,30 @@ final class NetworkManager<T: Requestable> {
                     observer.onCompleted()
                 }
             return Disposables.create {}
+        }
+    }
+}
+
+final class MockMapViewRepository: RequestProtocol {
+    func request<T: Decodable>(endPoint: Requestable) -> Observable<T> {
+        return Observable.create { observer in
+            let filename = "publicToilet.json"
+            guard let file = Bundle.main.url(forResource: filename, withExtension: nil) else {
+                observer.onError(NetworkError.emptyData)
+                return Disposables.create {}
+            }
+            guard let data = try? Data(contentsOf: file) else {
+                observer.onError(NetworkError.emptyData)
+                return Disposables.create {}
+            }
+            guard let decodedData = try? JSONDecoder().decode(T.self, from: data) else {
+                observer.onError(NetworkError.failToDecode)
+                return Disposables.create {}
+            }
+            observer.onNext(decodedData)
+            observer.onCompleted()
+
+            return Disposables.create()
         }
     }
 }
---
+++ favoriteToilet/favoriteToilet/Source/Model/API/RequestProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  RequestProtocol.swift
+//  favoriteToilet
+//
+//  Created by dale on 2022/07/27.
+//
+
+import Foundation
+import RxSwift
+
+protocol RequestProtocol {
+    func request<T: Decodable>(endPoint: Requestable) -> Observable<T>
+}
