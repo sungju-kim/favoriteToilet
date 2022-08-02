76500f8285ceeed9de2644bb3c9389fcae9fb707
sungju-kim <wnwn0719@gmail.com>
2022-08-02 21:29:00 +0900

    Refactor: MockMapViewRepository Observable -> Single 반환하도록 수정

--- favoriteToilet/favoriteToilet/Source/Repository/MockMapViewRepository.swift
+++ favoriteToilet/favoriteToilet/Source/Repository/MockMapViewRepository.swift
@@ -9,24 +9,22 @@ import Foundation
 import RxSwift
 
 final class MockMapViewRepository: RequestProtocol {
-    func request<T: Decodable>(endPoint: Requestable) -> Observable<T> {
-        return Observable.create { observer in
+    func request<T: Decodable>(endPoint: Requestable) -> Single<T> {
+        return Single.create { observer in
             let filename = "publicToilet.json"
             guard let file = Bundle.main.url(forResource: filename, withExtension: nil) else {
-                observer.onError(NetworkError.emptyData)
+                observer(.failure(NetworkError.emptyData))
                 return Disposables.create {}
             }
             guard let data = try? Data(contentsOf: file) else {
-                observer.onError(NetworkError.emptyData)
+                observer(.failure(NetworkError.emptyData))
                 return Disposables.create {}
             }
             guard let decodedData = try? JSONDecoder().decode(T.self, from: data) else {
-                observer.onError(NetworkError.failToDecode)
+                observer(.failure(NetworkError.failToDecode))
                 return Disposables.create {}
             }
-            observer.onNext(decodedData)
-            observer.onCompleted()
-
+            observer(.success(decodedData))
             return Disposables.create()
         }
     }
