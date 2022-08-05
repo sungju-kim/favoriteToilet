fb7579cdd8904771d24abf3413396ba70ff51a11
sungju-kim <wnwn0719@gmail.com>
2022-08-05 19:12:16 +0900

    Feat: ToiletMapEntity toDomain 메소드 구현

--- favoriteToilet/favoriteToilet/Source/Model/Entity/ToiletMapEntity.swift
+++ favoriteToilet/favoriteToilet/Source/Model/Entity/ToiletMapEntity.swift
@@ -17,3 +17,31 @@ struct ToiletEntity: Codable {
     let address, oldAddress: String?
     let latitude, longitude: Double?
 }
+
+extension ToiletEntity {
+    func toDomain() -> Toilet? {
+        guard let latitude = latitude,
+              let longitude = longitude else { return nil }
+        if let address = address {
+            return Toilet(id: id,
+                   name: name,
+                   address: address,
+                   coordinate: Coordinate(latitude: latitude, longitude: longitude),
+                   starRate: StarRate(averageRate: 5.0),
+                          information: [Information(contents: type),
+                                        Information(contents: openTime)])
+        }
+
+        if let oldAddress = oldAddress {
+            return Toilet(id: id,
+                   name: name,
+                   address: oldAddress,
+                   coordinate: Coordinate(latitude: latitude, longitude: longitude),
+                   starRate: StarRate(averageRate: 5.0),
+                          information: [Information(contents: type),
+                                        Information(contents: openTime)])
+
+        }
+        return nil
+    }
+}
