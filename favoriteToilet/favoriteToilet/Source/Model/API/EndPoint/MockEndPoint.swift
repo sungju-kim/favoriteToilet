236819354e1ee14df6d987ccd0cc6f7ab542a15e
sungju-kim <wnwn0719@gmail.com>
2022-08-29 16:04:13 +0900

    Refactor: MoakData 삭제 및 MockEndPoint변경

--- favoriteToilet/favoriteToilet/Source/Model/API/EndPoint/MockEndPoint.swift
+++ favoriteToilet/favoriteToilet/Source/Model/API/EndPoint/MockEndPoint.swift
@@ -8,11 +8,41 @@
 import Foundation
 import Alamofire
 
-struct MockEndPoint: Requestable {
-    private(set) var baseURL: URL? = URL(string: "")
-    private(set) var path: String = ""
-    private(set) var url: URL? = URL(string: "")
-    private(set) var headers: HTTPHeaders = HTTPHeaders()
-    private(set) var parameter: [String: String] = [:]
-    private(set) var method: HTTPMethod = HTTPMethod(rawValue: "")
+enum MockEndPoint {
+    case toilets
+    case comments
+}
+
+extension MockEndPoint: Requestable {
+    var baseURL: URL? {
+        return URL(string: "http://localhost:3000/")
+    }
+    var path: String {
+        switch self {
+        case .toilets:
+            return "toilets"
+        case .comments:
+            return "comments"
+        }
+    }
+    var url: URL? {
+        return baseURL?.appendingPathComponent(path)
+    }
+    var headers: HTTPHeaders {
+        return [HTTPHeader.accept("application/json")]
+    }
+
+    var parameter: [String: String] {
+        switch self {
+        default:
+            return [:]
+        }
+    }
+
+    var method: HTTPMethod {
+        switch self {
+        case .toilets, .comments:
+            return .get
+        }
+    }
 }
